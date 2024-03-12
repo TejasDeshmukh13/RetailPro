722d9fcc92d5c4aa96ead271f3ea82696c482d7a
PriyankaB26 <145274707+PriyankaB26@users.noreply.github.com>
2024-03-12 16:30:20 +0530

    dashboard done

--- dashboard.py
+++ dashboard.py
@@ -48,7 +48,7 @@ class Expentory:
 			bd=5,
 			cursor="hand1"
 		).place(x=20, y=30)
-		Button(width=7, pady=0, text=':', bg='light blue', fg='black', border=3,
+		Button(width=7, pady=0, text='Settings', bg='light blue', fg='black', border=3,
 		       font=("times new roman", 15, "bold"),
 		       ).place(x=1240, y=20)
 		Button(width=7, pady=0, text='About Us', bg='light blue', fg='black', border=3,
